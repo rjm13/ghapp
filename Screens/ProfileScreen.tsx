fb371ad943ae75ef9bc4ec013c7daa139888731e
rjm13 <65667098+rjm13@users.noreply.github.com>
2021-07-04 09:19:35 -0500

    profile screen ui

--- Screens/ProfileScreen.tsx
+++ Screens/ProfileScreen.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
-import { Avatar, Chip, TouchableRipple, Switch } from 'react-native-paper';
+import { ScrollView, StyleSheet, Text, View, TouchableWithoutFeedback, Dimensions } from 'react-native';
+import { Avatar } from 'react-native-paper';
 import WinLossHeader from '../Components/WinLossHeader';
 import Feather from 'react-native-vector-icons/Feather';
-import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { API, graphqlOperation, Auth } from "aws-amplify";
 import { getUser } from '../src/graphql/queries';
+import {StatusBar} from 'expo-status-bar';
+import * as Animatable from 'react-native-animatable';
 
-import ActiveGameScroll from '../Components/ActiveGames';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
-
 
 const ProfileScreen = ({ navigation } : any) => {
 
@@ -44,10 +42,12 @@ const ProfileScreen = ({ navigation } : any) => {
   }, [])
 
   return (
-
-      <ScrollView>
-        <View style={ styles.container}>
-            <View style={{ paddingHorizontal: 20, height: 100, width: SCREEN_WIDTH, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+    <View>
+        <Animatable.View 
+            animation='bounceInDown' 
+            style={{marginBottom: 20, alignItems: "center", backgroundColor: '#155843', 
+            paddingBottom: 20, borderBottomRightRadius: 40, borderBottomLeftRadius: 40,}}>
+            <View style={{ paddingHorizontal: 20, height: 80, width: SCREEN_WIDTH, flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between'}}>
                 <TouchableWithoutFeedback onPress={() => navigation.goBack()} style={{padding: 5}}>
                     <Feather 
                         name='chevron-down'
@@ -55,195 +55,142 @@ const ProfileScreen = ({ navigation } : any) => {
                         color='#fff'
                     />
                 </TouchableWithoutFeedback> 
+                <TouchableWithoutFeedback onPress={() => navigation.goBack()} style={{padding: 5}}>
+                    <FontAwesome 
+                        name='share'
+                        size={20}
+                        color='#fff'
+                        style={{marginBottom: 5}}
+                    />
+                </TouchableWithoutFeedback> 
             </View>
-          <Avatar.Image
-                source={{ uri: user?.imageUri || 'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg'}}
-                size={120}
-                style={{ 
-                  elevation: 2,
-                  marginBottom: 4,
-                  alignSelf: 'center',
-                }}
-            />
+            <View style={{alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff', borderRadius: 100, height: 125, width: 125}}>
+                <Avatar.Image
+                    source={{ uri: user?.imageUri || 'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg'}}
+                    size={120}
+                    style={{alignSelf: 'center',}}
+                />
+            </View>
+            
             <View style={ styles.displaynamebox}>
-              <Text style={ styles.displayname }>
-                {!!user ? user.name : 'Player 1'}
-              </Text>
-          </View>
-          <Text style={ styles.username }>
-            { user?.status || 'The worst trash talker in the land'}
-          </Text>
-          <TouchableOpacity onPress={() => {navigation.navigate('EditProfile')}}>
-            <Chip textStyle={{  
-                      fontFamily: 'chalkboard-light',
-                      fontSize: 10,
-                      textTransform: 'uppercase',
-                      letterSpacing: 0.5,
-                      }} style={ styles.chip } >
-                  Edit
-            </Chip>
-          </TouchableOpacity>
-
-          {/* <View style={ styles.winloss }>
-            <WinLossHeader />
-          </View> */}
-
-        </View>
-
-        <View style={{ width: '90%', alignSelf: 'center'}}>
-          <Text style={ styles.activeHeader }>Active Games</Text>
-        </View>
-
-        <View>
-          <ActiveGameScroll />
-        </View>
-
-        <TouchableWithoutFeedback
-          onPress={() => {navigation.navigate('FriendsList')}}
-        >
-          <View style={ styles.share }>
-            <FontAwesome
-              name='users'
-              size={20}
-              color='#155843'
-            />
-              <Text style={ styles.shareText }>Friends</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <TouchableWithoutFeedback
-          onPress={() => {navigation.navigate('Favorites')}}
-        >
-          <View style={ styles.share }>
-            <AntDesign 
-              name='heart'
-              size={20}
-              color='red'
-            />
-              <Text style={ styles.shareText }>Favorites</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <TouchableWithoutFeedback
-          onPress={() => {navigation.navigate('Framed')}}
-        >
-          <View style={ styles.share }>
-            <FontAwesome 
-              name='trophy'
-              size={25}
-              color='gold'
-            />
-              <Text style={ styles.shareText }>Framed Games</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <View style={ styles.share }>
-            <Feather 
-              name='share-2'
-              size={20}
-              color='#155843'
-            />
-            <Text style={ styles.shareText }>Stats</Text>
-        </View>
-
-        <TouchableWithoutFeedback
-          onPress={handleSignOut}
-        >
-          <View style={{ paddingBottom: 24}}>
-            <View style={ styles.signout }>
-              {/* <FontAwesome
-                name='sign-out'
-                size={25}
-                color='#155843'
-              /> */}
-                <Text style={ styles.shareText }>Sign Out</Text>
+                <Text style={ styles.displayname }>
+                    {!!user ? user.name : 'Player 1'}
+                </Text>
             </View>
-          </View>
-        </TouchableWithoutFeedback>
-    </ScrollView>
+            <Text style={ styles.username }>
+                { user?.status || 'The worst trash talker in the land'}
+            </Text>
+
+            {/* <View style={ styles.winloss }>
+                <WinLossHeader />
+            </View> */}
+        </Animatable.View>
+
+        <ScrollView>
+            <View>
+                <TouchableWithoutFeedback onPress={() => {navigation.navigate('FriendsList')}}>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Friends</Text>
+                        <Feather
+                            name='chevron-right'
+                            size={20}
+                            color='#000'
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+
+                <TouchableWithoutFeedback onPress={() => {navigation.navigate('Favorites')}}>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Favorites</Text>
+                        <Feather 
+                            name='chevron-right'
+                            size={20}
+                            color='#000'
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+
+                <TouchableWithoutFeedback onPress={() => {navigation.navigate('Framed')}}>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Archived</Text>
+                        <Feather 
+                        name='chevron-right'
+                        size={20}
+                        color='#000'
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+
+                <TouchableWithoutFeedback onPress={() => {navigation.navigate('Framed')}}>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Statistics</Text>
+                        <Feather 
+                            name='chevron-right'
+                            size={20}
+                            color='#000'
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+
+                <TouchableWithoutFeedback onPress={() => {navigation.navigate('EditProfile')}}>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Edit Profile</Text>
+                        <Feather 
+                            name='chevron-right'
+                            size={20}
+                            color='#000'
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+
+                <TouchableWithoutFeedback>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Settings</Text>
+                        <Feather 
+                            name='chevron-right'
+                            size={20}
+                            color='#000'
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+        
+                <TouchableWithoutFeedback onPress={handleSignOut}>
+                    <View style={ styles.box }>
+                        <Text style={ styles.boxText }>Sign Out</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
+        </ScrollView>
+        <StatusBar style='light'/>
+    </View>
   );
 }
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    marginBottom: 20,
-    justifyContent: "flex-start",
-    alignSelf: 'center',
-    alignItems: "center",
-    flexDirection: 'column',
-    width: '100%',
-    elevation: 1,
-    backgroundColor: '#155843',
-    paddingBottom: 20,
-    borderBottomRightRadius: 40,
-    borderBottomLeftRadius: 40,
-  },
-  share: {
-    marginTop: 12, 
-    //marginBottom: 2,
+  box: {
     justifyContent: 'space-between',
     alignSelf: 'center',
     alignItems: 'center',
     flexDirection: 'row',
     width: '90%',
     height: 60,
-    elevation: 1,
-    backgroundColor: '#fff',
-    //borderRadius: 8,
-    padding: 32,
+    padding: 10,
+  },
+  boxText: {
+    fontFamily: 'chalkboard-regular',
+    fontSize: 16,
   },
   displayname: {
     fontSize: 20,
     fontFamily: 'chalkboard-bold',
-    marginRight: 8,
     color: '#fff',
   },
   username: {
     fontSize: 16,
     fontFamily: 'chalkboard-light',
-    paddingTop: 8,
+    paddingTop: 10,
     color: '#fff'
-  },
-  winloss: {
-    margin: 16,
-  },
-  shareText: {
-    fontFamily: 'chalkboard-regular',
-    fontSize: 16,
-    letterSpacing: 1,
-  },
-  activeHeader: {
-    fontFamily: 'chalkboard-bold',
-    fontSize: 18,
-
-  },
-  activeContent: {
-    width: 100,
-    height: 150,
-    elevation: 1,
-  },
-  displaynamebox: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-  },
-  chip: {
-    marginTop: 8,
-  },
-  signout: {
-    marginTop: 12, 
-    //marginBottom: 2,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    width: '60%',
-    height: 60,
-    //elevation: 1,
-    //backgroundColor: '#fff',
-    borderRadius: 8,
-    padding: 32,
   },
 })
